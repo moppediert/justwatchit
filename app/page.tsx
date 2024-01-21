59a5da1d21cc4f0e16eada7b3c56b3821a55d02c
moppediert <moppediert@gmail.com>
2024-01-21 21:50:38 +0100

    adjust dimension of player

--- app/page.tsx
+++ app/page.tsx
@@ -177,7 +177,7 @@ export default function Home() {
       <Dialog open={playing} onOpenChange={setPlaying}>
         <DialogContent
           onPointerDownOutside={(e) => e.preventDefault()}
-          className="max-w-full w-[1080px] h-[720px] p-0 border-0"
+          className="max-w-full w-3/5 h-3/5 p-0 border-0"
         >
           <iframe
             className="h-full w-full"
