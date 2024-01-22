7fbb43e83c877666ddcd99c5d0dd68fdeb4ca0cc
moppediert <moppediert@gmail.com>
2024-01-22 08:16:19 +0100

    fix unused dependency

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export const metadata: Metadata = {
   title: "Just Watch It",
@@ -13,12 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <GoogleOAuthProvider
-      clientId={process.env.NEXT_PUBLIC_OAUTH2_CLIENT_ID as string}
-    >
-      <html lang="en" className="h-full dark overscroll-none">
-        <body className="h-full py-8 overscroll-none">{children}</body>
-      </html>
-    </GoogleOAuthProvider>
+    <html lang="en" className="h-full dark overscroll-none">
+      <body className="h-full py-8 overscroll-none">{children}</body>
+    </html>
   );
 }
